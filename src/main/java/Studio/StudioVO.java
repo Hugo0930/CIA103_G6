2149f262a17dc5a07a19647b6ec91f7e50627530
little-luo <face000623@gmail.com>
2024-10-23 16:05:41 +0800

    firstCommit

--- src/main/java/Studio/StudioVO.java
+++ src/main/java/Studio/StudioVO.java
@@ -3,5 +3,17 @@ package Studio;
 public class StudioVO {
 	String name;
 	String loc;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
 	
 }
