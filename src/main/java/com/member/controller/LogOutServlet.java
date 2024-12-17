2c57652dec04ff735e1b599fc0136abf9ef92f38
Yang <yang88991116@gmail.com>
2024-12-17 23:47:40 +0800

    commit logout web.xml

---
+++ src/main/java/com/member/controller/LogOutServlet.java
@@ -0,0 +1,21 @@
+package com.member.controller;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogOutServlet extends HttpServlet {
+	@Override
+	
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		session.removeAttribute("mem");
+		resp.sendRedirect(req.getContextPath()+"/front-end/login.jsp");
+	}
+}
